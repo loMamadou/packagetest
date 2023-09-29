09a7b1e935d564856298b92df19b44ccf746c61f
mlo00 <mamadou.lo@lmdsio.fr>
2023-09-29 15:04:15 +0200

    V1

---
+++ src/MontantValidator.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mlo\Testmontantrais;
+
+class MontantValidator
+{
+    public static function validerMontant($montant)
+    {
+        return is_numeric($montant) && $montant>=0.01 && $montant <= 200;
+    }
+}
